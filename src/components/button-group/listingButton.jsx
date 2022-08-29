8dc26c5dadfb2214df49c42ee67331241d124e14
Osman Ordu <orduosmann@gmail.com>
2022-08-29 15:33:47 +0300

    --

--- src/components/button-group/listingButton.jsx
+++ src/components/button-group/listingButton.jsx
@@ -2,14 +2,26 @@ import './listingButtons.styles.scss'
 import { itemData } from '../../mock/itemData';
 
 const listingButton = ({ setFilter }) => {
-    const filterRisingPrice = (e) => {
+
+    const filterRisingPrice = () => {
         const filterPrice = itemData.sort((a, b) => parseFloat(a.item.price) - parseFloat(b.item.price), Number);
         setFilter(filterPrice)
         setFilter([])
     }
-    const filterDecreasingPrice = (e) => {
+    const filterDecreasingPrice = () => {
         const filterPrice = itemData.sort((a, b) => parseFloat(b.item.price) - parseFloat(a.item.price), Number);
         setFilter(filterPrice)
+        setFilter([])
+    }
+    const filterLetter = () => {
+        return itemData.sort((a, b) => a.item.content.localeCompare(b.item.content))
+    }
+    const filterAlphabetically = () => {
+        setFilter(filterLetter())
+        setFilter([])
+    }
+    const filterReverseAlpabetically = () => {
+        setFilter(filterLetter().reverse())
         setFilter([])
     }
 
@@ -22,17 +34,17 @@ const listingButton = ({ setFilter }) => {
                     Fiyata göre(Artan)
                 </span>
             </button>
-            <button onClick={ filterDecreasingPrice} className='btn'>
+            <button onClick={filterDecreasingPrice} className='btn'>
                 <span>
                     Fiyata göre(Azalan)
                 </span>
             </button>
-            <button className='btn'>
+            <button onClick={filterAlphabetically} className='btn'>
                 <span>
                     Ürün Adına Göre (A-Z)
                 </span>
             </button>
-            <button className='btn'>
+            <button onClick={filterReverseAlpabetically} className='btn'>
                 <span>
                     Ürün Adına Göre (Z-A)
                 </span>
